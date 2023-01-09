13c4c0da833de7c7556609749db0bafe01a7de1f
3swordman <49294757+3swordman@users.noreply.github.com>
2023-01-09 21:55:15 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const blockUsers = []
+
+on("receive", function (req) {
+  if (blockUsers.includes(req.usrname)) return null
+  return req
+})
+
+on("send", function (req) {
+  if (!req.msg.startswith("/mute")) {
+    return req
+  }
+  blockUsers.push(req.msg.slice(6))
+  return null
+})
